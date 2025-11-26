8702e762bac2f02f017cbdd32bad53cc38c4468c
ACLIFF123 <aaronclifford26@gmail.com>
2025-11-26 22:41:52 +0000

    Feat: Naive Bayes and Random Forest model options to model_ pipeline_builder, allow to change model and see which works best intially with data before tuning

--- src/models/model_pipeline_builder.py
+++ src/models/model_pipeline_builder.py
@@ -1,6 +1,8 @@
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.ensemble import RandomForestClassifier
 
 def build_model_pipeline(model_type="logistic"):
     if model_type == "logistic":
@@ -8,5 +10,18 @@ def build_model_pipeline(model_type="logistic"):
             ("tfidf", TfidfVectorizer(stop_words="english")),
             ("clf", LogisticRegression(max_iter=1000))
         ])
+
+    elif model_type == "naive_bayes":
+        return Pipeline([
+            ("tfidf", TfidfVectorizer(stop_words="english")),
+            ("clf", MultinomialNB())
+        ])
+
+    elif model_type == "random_forest":
+        return Pipeline([
+            ("tfidf", TfidfVectorizer(stop_words="english")),
+            ("clf", RandomForestClassifier(n_estimators=200))
+        ])
+
     else:
         raise ValueError(f"Unsupported model type: {model_type}")
--- test/test_model_pipeline_builder.py
+++ test/test_model_pipeline_builder.py
@@ -1,6 +1,9 @@
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
 from src.models.model_pipeline_builder import build_model_pipeline
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.ensemble import RandomForestClassifier
+
 
 def test_build_model_pipeline():
     pipeline = build_model_pipeline("logistic")
@@ -17,3 +20,20 @@ def test_pipeline_can_train():
     y = ["greeting", "statement"]
 
     pipeline.fit(X, y)
+
+def test_build_naive_bayes_pipeline():
+    pipeline = build_model_pipeline("naive_bayes")
+
+    assert isinstance(pipeline, Pipeline)
+    assert "tfidf" in pipeline.named_steps
+    assert "clf" in pipeline.named_steps
+    assert isinstance(pipeline.named_steps["clf"], MultinomialNB)
+
+
+def test_build_random_forest_pipeline():
+    pipeline = build_model_pipeline("random_forest")
+
+    assert isinstance(pipeline, Pipeline)
+    assert "tfidf" in pipeline.named_steps
+    assert "clf" in pipeline.named_steps
+    assert isinstance(pipeline.named_steps["clf"], RandomForestClassifier)
